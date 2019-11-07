dfce358f0caa4af6213ecfcb49b6072e6cf5a24c
celso.alineri <cel.alineri@hotmail.com>
2019-11-06 22:12:06 -0300

    criado layout inicial

--- app/src/main/java/com/br/senac/ppdm/cartagena/MainActivity.java
+++ app/src/main/java/com/br/senac/ppdm/cartagena/MainActivity.java
@@ -3,12 +3,25 @@ package com.br.senac.ppdm.cartagena;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnJogar;
+    Button btnOpcoes;
+    Button btnSair;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnJogar = findViewById(R.id.btnJogar);
+        btnOpcoes = findViewById(R.id.btnOpcoes);
+        btnSair = findViewById(R.id.btnSair);
+
+        
+
     }
 }
